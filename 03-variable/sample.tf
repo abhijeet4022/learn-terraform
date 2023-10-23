7a98ecc9ff3390ee67b9fcd7e4476be62fd3c032
abhijeet4022 <abhijeet4022@gmail.com>
2023-10-23 10:40:49 +0530

    Terraform: Variable

--- 03-variable/sample.tf
+++ 03-variable/sample.tf
@@ -1,7 +1,40 @@
+#Plain Value
 variable "fruit_name" {
   default = "apple"
 }
 
 output "fruit_name" {
   value = var.fruit_name
+}
+
+#List of item
+variable "fruits_name" {
+  default = [
+  "apple",
+  "banana"
+  ]
+}
+
+// default = ["apple", "banana"]
+
+#Map value
+variable "fruit_stock" {
+  default = {
+    apple = 100
+    banana = 200
+  }
+}
+
+#Map value
+variable "fruit_stock_with_price" {
+  default = {
+    apple = {
+      stock = 100
+      price = 30
+    }
+    banana = {
+      stock = 200
+      price = 5
+    }
+  }
 }
