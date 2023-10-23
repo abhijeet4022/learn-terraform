fe068e974ff04ea33ef1b9d9cea1e6af1aadad1d
abhijeet4022 <abhijeet4022@gmail.com>
2023-10-23 09:11:36 +0530

    Terraform: Roboshop Project - V1

---
+++ roboshop-v1/roboshop.tf
@@ -0,0 +1,11 @@
+provider "aws" {}
+
+resource "aws_instance" "frontend" {
+  ami = "ami-03265a0778a880afb"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-01746b650f07ff560"]
+
+  tags = {
+    name = frontend
+  }
+}
