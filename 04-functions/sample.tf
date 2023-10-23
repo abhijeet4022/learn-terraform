7a9fcbb42a8a2f6015c52d7b8c9d783fc01e44fb
abhijeet4022 <abhijeet4022@gmail.com>
2023-10-23 11:54:59 +0530

    Terraform: Function

--- 04-functions/sample.tf
+++ 04-functions/sample.tf
@@ -6,3 +6,13 @@ output "fruit_name" {
   //value = var.fruits_name[2]
   value = element(var.fruits_name, 2)
 }
+
+variable "fruit_stock" {
+  default = {
+    apple = 100
+  }
+}
+
+output "fruit_stock" {
+  value = try(var.fruit_stock["banana"], 0)
+}
