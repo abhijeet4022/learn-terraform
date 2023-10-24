c73b798247685d9463c7cd1d8c8e9b26676fe65f
abhijeet4022 <abhijeet4022@gmail.com>
2023-10-24 09:35:51 +0530

    Terraform: Loops - Count

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -5,18 +5,22 @@ variable "components" {
 }
 
 resource "aws_instance" "instances" {
-
   count                  = length(var.components)
   ami                    = "ami-03265a0778a880afb"
   instance_type          = "t3.small"
   vpc_security_group_ids = ["sg-01746b650f07ff560"]
-
+  tags = {
+    Name = element(var.components, count.index)
+    // Name = var.components[count.index]
+  }
 }
 
-  output "element"  {
-    // Name = var.components[count.index]
-    // Name = element(var.components, count.index)
-    value = element(["frontend", "mongodb"], 5)
-  }
+
+#  output "element"  {
+#    value = element(["frontend", "mongodb"], 0)
+#    value = element(["frontend", "mongodb"], 1)
+#    value = element(["frontend", "mongodb"], 2)
+#    value = element(["frontend", "mongodb"], 3)
+#  }
 
 
