32a395a01caf8f2e6adb43760f1ede499e5c0a9f
abhijeet4022 <abhijeet4022@gmail.com>
2023-10-25 09:40:24 +0530

    Terraform: roboshop-v2

--- roboshop-v2/roboshop.tf
+++ roboshop-v2/roboshop.tf
@@ -41,7 +41,7 @@ variable "components" {
 # Frontend Configuration
 resource "aws_instance" "instance" {
   for_each = var.components
-  ami = data.aws_ami.ami
+  ami = data.aws_ami.ami.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = var.sg
 
