979cd24ebc4a6219dc4a9e4646878d78dce689e4
abhijeet4022 <abhijeet4022@gmail.com>
2023-10-25 09:34:15 +0530

    Terraform: roboshop-v2

--- roboshop-v2/roboshop.tf
+++ roboshop-v2/roboshop.tf
@@ -1,7 +1,13 @@
 provider "aws" {}
 
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+  owners = ["973714476881"]
+}
+
 variable "ami" {
-  default = "ami-03265a0778a880afb"
+  default = data.aws_ami.ami
 }
 
 variable "instance_type" {
@@ -33,22 +39,22 @@ variable "components" {
 }
 
 # Frontend Configuration
-resource "aws_instance" "frontend" {
+resource "aws_instance" "instance" {
   for_each = var.components
   ami = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = var.sg
 
   tags = {
-    Name = lookup(each.key, "name", null )
+    Name = lookup(each.value, "name", null )
   }
 }
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "record" {
   for_each = var.components
   zone_id = var.zone
-  name    = "${lookup(each.key,)}-dev.learntechnology.tech"
+  name    = "${lookup(each.value, "name", null)}.learntechnology.tech"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.frontend.private_ip]
+  records = [lookup(lookup(aws_instance.instance, each.key, null), "private_ip", null)]
 }
 
