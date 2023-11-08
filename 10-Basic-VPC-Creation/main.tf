4d865e6436c07210b1d0bd6b39b7e0ecaa323aeb
abhijeet4022 <abhijeet4022@gmail.com>
2023-11-08 11:11:31 +0530

    Terraform: Roboshop-App Terraform

---
+++ 10-Basic-VPC-Creation/main.tf
@@ -0,0 +1,35 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnets[count.index]
+  tags = {
+    Name = "Public-${count.index+1}"
+  }
+}
+
+resource "aws_subnet" "app" {
+  count = length(var.app_subnets)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnets[count.index]
+  tags = {
+    Name = "App-${count.index+1}"
+  }
+}
+
+resource "aws_subnet" "db" {
+  count = length(var.db_subnets)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnets[count.index]
+  tags = {
+    Name = "DB-${count.index+1}"
+  }
+}
+
+
